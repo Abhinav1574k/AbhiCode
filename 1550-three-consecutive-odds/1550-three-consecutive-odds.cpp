7b5551fbeebeec069e18e129ab7a97a70c9eed2a
Abhinav1574k <kanha1574k@gmail.com>
2026-09-02 02:01:10 +0530

    Time: 0 ms (100%), Space: 11.8 MB (61.73%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int oddCount = 0;
+
+        for(int num : arr){
+            if(num % 2 != 0){
+                oddCount++;
+                if(oddCount == 3){
+                    return true;
+                }
+            }
+            else{
+                oddCount = 0;
+            }
+        }
+        return false;
+    }
+};
