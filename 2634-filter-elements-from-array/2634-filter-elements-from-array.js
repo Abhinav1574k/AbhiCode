231b357151a68d57a2b8d593a41d23100abb1deb
Abhinav1574k <kanha1574k@gmail.com>
2026-09-11 00:52:39 +0530

    Time: 42 ms (64.32%), Space: 54.7 MB (8.22%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    filteredArr = [];
+    let filteredArr = [];
 
     for(let i = 0; i < arr.length; i++){
         if(fn(arr[i], i)){
