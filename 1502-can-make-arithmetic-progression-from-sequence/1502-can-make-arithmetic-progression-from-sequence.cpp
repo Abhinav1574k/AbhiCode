bd465b33ea453e7570fcd8bbbe8da244d8d48661
Abhinav1574k <kanha1574k@gmail.com>
2026-09-19 03:01:12 +0530

    Time: 0 ms (100%), Space: 12.8 MB (16.77%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -6,7 +6,7 @@ public:
         int d = arr[1] - arr[0];
 
         for(int i = 1; i < arr.size(); i++){
-            if(abs(arr[i] - arr[i - 1]) != d){
+            if(arr[i] - arr[i - 1] != d){
                 return false;
             }
         }
